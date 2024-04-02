f204acff5ca626469a3ef0fc41f8cb220a8105cf
hagmal <mahag2193@oslomet.no>
2024-04-02 14:38:24 +0200

    Laget controller og konstruktør/get/set-metoder

--- src/main/java/DATA1700/oblig2/Billett.java
+++
@@ -1,5 +0,0 @@
-package DATA1700.oblig2;
-
-public class Billett {
-    private String
-}
--- src/main/java/DATA1700/oblig2/Controller.java
+++ src/main/java/DATA1700/oblig2/Controller.java
@@ -1,9 +1,21 @@
 package DATA1700.oblig2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class Controller {
-
+    public final List<Kinobillett> alleBilletter = new ArrayList<>();
+    @PostMapping("/lagre")
+    public void lagreBillett (Kinobillett innBillett) {
+        alleBilletter.add(innBillett);
+    }
+    @GetMapping("/hentBilletter")
+    public List<Kinobillett> hentBilletter() {
+        return alleBilletter;
+    }
 }
